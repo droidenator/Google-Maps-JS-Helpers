3593a86af650f94b1342768f557451ada242f0e7
droidenator <scorrales@gmail.com>
2013-11-14 22:02:19 -0600

    Adding infowindow setup to init function

--- buster.js
+++ buster.js
@@ -15,25 +15,31 @@ var buster = {
  * Initialize google map variables and bind to window load
  *
  * @param {string} ID of DOM object to turn into a google map
- * @param {object} Contains lat and lng. Should be in format center.lat, center.lng. 
+ * @param {object} Contains lat and lng. Should be in format center.lat, center.lng.
+ * @param {boolean} Whether or not to create the infowindow object
  */
-buster.init = function(mapId, center){
+buster.init = function(mapId, center, createInfo){
 	this.mapId = mapId;
 	this.center = center;
 
-	google.maps.event.addDomListener(window, 'load', buster.createMap);
+	google.maps.event.addDomListener(window, 'load', function(){
+        buster.createMap(createInfo);
+    });
 }
 
 /**
  * Create map and set center
  */
-buster.createMap = function(){
+buster.createMap = function(createInfo){
 	//Create map using supplied ID
 	buster.map = new google.maps.Map(document.getElementById(buster.mapId), buster.mapOptions);
 	
 	//Set center. @TODO - add support for using current location or geocoding of supplied address
 	var pos = new google.maps.LatLng(buster.center.lat, buster.center.lng);
-	buster.map.setCenter(pos);	
+	buster.map.setCenter(pos);
+
+    //Create info window
+    if(createInfo) buster.infoWindow = new google.maps.InfoWindow();
 }
 
 /**
